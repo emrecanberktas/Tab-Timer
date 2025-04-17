8aa27ecd1836f22a531e973731138064746b644a
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-04-17 14:54:08 +0300

    Chore: not want to use (temporarly)

--- content.js
+++ content.js
@@ -1,12 +1,34 @@
-chrome.runtime.sendMessage({ action: "getTabTime", tabId: chrome.runtime.id }, function (response) {
-    var tooltip = document.createElement("div");
-    tooltip.className = "tab-timer-tooltip";
-    tooltip.textContent = "Bu sekme ".concat(formatTime(response.time), " a\u00E7\u0131k");
-    document.body.appendChild(tooltip);
-    function formatTime(ms) {
-        var seconds = Math.floor(ms / 1000);
-        var minutes = Math.floor(seconds / 60);
-        var hours = Math.floor(minutes / 60);
-        return "".concat(hours, "s ").concat(minutes % 60, "dk ").concat(seconds % 60, "s");
-    }
-});
+// chrome.runtime.sendMessage(
+//   { action: "getTabTime", tabId: chrome.runtime.id },
+//   function (response) {
+//     var tooltip = document.createElement("div");
+//     tooltip.className = "tab-timer-tooltip";
+
+//     function updateTooltip() {
+//       chrome.runtime.sendMessage(
+//         { action: "getTabTime", tabId: chrome.runtime.id },
+//         function (response) {
+//           tooltip.textContent = "This tab has been open for ".concat(
+//             formatTime(response.time)
+//           );
+//         }
+//       );
+//     }
+
+//     // Update immediately and then every second
+//     updateTooltip();
+//     setInterval(updateTooltip, 1000);
+
+//     document.body.appendChild(tooltip);
+
+//     function formatTime(ms) {
+//       var seconds = Math.floor(ms / 1000);
+//       var minutes = Math.floor(seconds / 60);
+//       var hours = Math.floor(minutes / 60);
+//       return ""
+//         .concat(hours, "h ")
+//         .concat(minutes % 60, "m ")
+//         .concat(seconds % 60, "s");
+//     }
+//   }
+// );
